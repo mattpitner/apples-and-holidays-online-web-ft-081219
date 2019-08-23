3fea646e4c3bea6c5b7239e650fbaf02ff2f67e6
Matt Pitner <mattpitner09@gmail.com>
2019-08-23 16:13:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,9 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-holiday_hash.map {|holiday, supplies| holiday if supplies.include?("BBQ")}
+holiday_hash.map do |season, holiday|
+  holiday.map do |holiday, supply|
+if supplies.include?("BBQ")}
   
   end 
 
